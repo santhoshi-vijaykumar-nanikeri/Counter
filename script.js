5b2ef6619ab37c2133a120b130babffd4f38a27d
Santhoshi Vijaykumar Nanikeri <santhoshi.nanikeri@gmail.com>
2024-12-16 20:14:03 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,32 @@
+const plusBtn = document.querySelector(".plus-btn");
+const minusBtn = document.querySelector(".minus-btn");
+const counter = document.querySelector(".counter");
+const changeVal = document.querySelector(".change-val");
+const reset = document.querySelector(".reset");
+
+// plusBtn.addEventListener("click", () => {
+//   const counterVal = parseInt(counter.innerText);
+//   counter.innerText = counterVal + parseInt(changeVal.value);
+//   //   console.log("button clicked   ");
+// });
+
+// minusBtn.addEventListener("click", () => {
+//   const counterVal = parseInt(counter.innerText);
+//   counter.innerText = counterVal - parseInt(changeVal.value);
+// });
+
+// reset.addEventListener("click", () => {
+//   counter.innerText = 0;
+// });
+plusBtn.addEventListener("click", () => {
+  const counterVal = parseInt(counter.innerText);
+  counter.innerText = counterVal + parseInt(changeVal.value);
+});
+minusBtn.addEventListener("click", () => {
+  const counterVal = parseInt(counter.innerText);
+  counter.innerText = counterVal - parseInt(changeVal.value);
+});
+
+reset.addEventListener("click", () => {
+  counter.innerText = 0;
+});
